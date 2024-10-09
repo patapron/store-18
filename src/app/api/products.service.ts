c411311bf1ae99c410173be2968f502530037ef5
patapron <pronick@gmail.com>
2024-10-09 14:24:16 +0200

    changed subscribe by rxmethod

--- src/app/api/products.service.ts
+++ src/app/api/products.service.ts
@@ -1,37 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import {
+  DestroyRef,
   EnvironmentInjector,
   inject,
   Injectable,
   runInInjectionContext,
   signal,
 } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { environment } from '@envs/environment';
 import { Product } from '@shared/models/product.interface';
 import { tap, Observable, map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
-  public products = signal<Product[]>([]);
+  // public products = signal<Product[]>([]);
   private readonly _http = inject(HttpClient);
   private readonly _endPoint = environment.apiURL;
   private readonly _injector = inject(EnvironmentInjector);
+  private _destroyRef = inject(DestroyRef);
 
-  constructor() {
-    this.getProducts();
-  }
+  // constructor() {
+  //   this.getProducts();
+  // }
 
-  public getProducts(): void {
-    this._http
+  // public getProducts(): void {
+  //   this._http
+  //     .get<Product[]>(`${this._endPoint}/products/?sort=desc`)
+  //     .pipe(
+  //       map((products: Product[]) =>
+  //         products.map((product: Product) => ({ ...product, qty: 1 }))
+  //       ),
+  //       tap((data: Product[]) => this.products.set(data))
+  //     )
+  //     .subscribe();
+  // }
+
+  public getProducts() {
+    return this._http
       .get<Product[]>(`${this._endPoint}/products/?sort=desc`)
       .pipe(
+        takeUntilDestroyed(this._destroyRef),
         map((products: Product[]) =>
           products.map((product: Product) => ({ ...product, qty: 1 }))
-        ),
-        tap((data: Product[]) => this.products.set(data))
-      )
-      .subscribe();
+        )
+      );
   }
 
   public getProductById(id: number) {
